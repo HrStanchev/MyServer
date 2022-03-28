baf294fa7ca249179b5d54be91bbcc2050c7cbe6
HrStanchev <93995443+HrStanchev@users.noreply.github.com>
2022-03-28 13:26:56 +0300

    Add files via upload

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyServer.BL.Services;
+using MyServer.BL.Interfaces;
+using MyServer.DL;
+using MyServer.Models;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+
+namespace MyServer.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private readonly IRabbitMqService _rabbitMq;
+
+        public PersonController(ILogger<PersonController> logger, IRabbitMqService rabbitMq)
+        {
+            _logger = logger;
+            _rabbitMq = rabbitMq;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendPerson([FromBody] Person p)
+        {
+            await _rabbitMq.PublishPersonAsync(p);
+
+            return Ok();
+        }
+
+        //[HttpPost("InsertInMongo")]
+        //public async Task<IActionResult> InsertPerson([FromBody] Person p)
+        //{
+        //    await _personRepository
+        //}
+
+    }
+}
