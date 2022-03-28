95e4915165a3938b81da6758e9db5e2163345841
HrStanchev <93995443+HrStanchev@users.noreply.github.com>
2022-03-28 13:25:26 +0300

    BL

---
+++ MyServer.BL/Interfaces/IRabbitMqService.cs
@@ -0,0 +1,10 @@
+﻿using MyServer.Models;
+using System.Threading.Tasks;
+
+namespace MyServer.BL.Interfaces
+{
+    public interface IRabbitMqService
+    {
+        Task PublishPersonAsync(Person p);
+    }
+}
---
+++ MyServer.BL/Interfaces/RabbitMqServiceConsumer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace MyServer.BL.Interfaces
+{
+    public class RabbitMqServiceConsumer : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Task.Factory.StartNew(() =>
+            {
+
+            }, cancellationToken);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
